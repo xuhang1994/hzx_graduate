6d91ee211ca00c60818b47c264c460ef33658dd9
Master Shi <zidane765@hotmail.com>
2018-01-05 16:42:59 +0800

    做完了

--- dayahead.py
+++ dayahead.py
@@ -37,9 +37,11 @@ for e in [1,0.8,0.6,0.4,0.2]:
     print('电需求响应' + str(100*e) + '%：'+ str(value(responseStrategy.objective)))
     #获取结果
     res_result = optimizationModel.retriveResult(microgrid_data,case,responseStrategy)
+    optimizationModel.extendedResult(res_result)
     '电关口功率曲线画图，并保存excle'
     plt.plot(res_result['购电功率'], label=(str(100 * e) + '%电需求响应',))
     plt.legend()
+    plt.show()
     res_result.to_excel(writer,sheet_name = str(e))
 writer.save()
 plt.show()
@@ -54,9 +56,11 @@ for e in [1,0.8,0.6,0.4,0.2]:
     print('热需求响应' + str(100 * e) + '%：' + str(value(responseStrategy.objective)))
     # 获取结果
     res_result = optimizationModel.retriveResult(microgrid_data, case, responseStrategy)
+    optimizationModel.extendedResult(res_result)
     '''热关口功率曲线画图，并保存excel'''
     plt.plot(res_result['购热功率'], label=str(100 * e) + '%热需求响应')
     plt.legend()
+    plt.show()
     res_result.to_excel(writer, sheet_name=str(e))
 writer.save()
 plt.show()
